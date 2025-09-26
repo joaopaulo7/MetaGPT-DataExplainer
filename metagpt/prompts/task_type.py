41c57b6ca50c737f7d917156703c7f1380a27bda
joaopaulo7 <joaopauloff7@hotmail.com>
2025-09-25 21:34:32 -0300

    Reduce traceback

--- metagpt/prompts/task_type.py
+++ metagpt/prompts/task_type.py
@@ -18,7 +18,7 @@ The current task is about data preprocessing, please note the following:
 - Only encode or scale necessary columns to allow for potential feature-specific engineering tasks (like time_extract, binning, extraction, etc.) later.
 - Each step do data preprocessing to train, must do same for test, in the same cell but separately, to prevent data leakage.
 - Remember to correctly encode categorical and numerical data.
-- Pay special attention to dates adn time data, if present.
+- Pay special attention to dates and time data, if present.
 - To avoid creating excessive features, always check the amount of unique categorical values in the feature before encoding.
 - Only encode categorical values if they are significantly represented in the data. 
 - Always copy the DataFrame before processing it and use the copy to process.
--- metagpt/strategy/explainer_planner.py
+++ metagpt/strategy/explainer_planner.py
@@ -68,7 +68,7 @@ def _clean_outputs(outputs):
                 'ename': output['ename'],
                 'evalue': _remove_ansi_colors(output['evalue']),
                 # only get the most important traceback
-                'traceback': [_remove_ansi_colors(tb) for tb in output['traceback'][:4] + output['traceback'][-3:]]})
+                'traceback': [_remove_ansi_colors(tb) for tb in output['traceback'][:2] + output['traceback'][-1:]]})
     return new_outputs
 
 
